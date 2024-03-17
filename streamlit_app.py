0ee6c1be3949e54f02ddc46cb4f652e994f96291
burrybaum <114881682+burrybaum@users.noreply.github.com>
2024-03-17 09:52:11 +0000

    create initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain.llms import OpenAI
-st.set_page_config(page_title="🦜🔗 Quickstart App")
+st.set_page_config(page_title="🦜🔗 Start app App")
 st.title('🦜🔗 Quickstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
@@ -10,7 +10,7 @@ def generate_response(input_text):
   st.info(llm(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  text = st.text_area('Enter text:', 'pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
